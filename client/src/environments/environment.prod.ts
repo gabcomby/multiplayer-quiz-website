2adf1598da941a4af637acfb82af3df269dd1fca
chapsag <chapsag@gmail.com>
2024-02-25 09:38:51 -0500

    [REFACTORING | PE] Mise à jour des services de question et de socket

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: 'myServerURL', // TODO: Add remote server URL
+    serverUrl: 'https://ec2-18-191-191-125.us-east-2.compute.amazonaws.com',
 };
