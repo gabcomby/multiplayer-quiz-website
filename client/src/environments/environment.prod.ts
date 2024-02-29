9c51d65f5c01d8df6f72e28892bc4c2db358aa2f
Alexane123 <112894037+Alexane123@users.noreply.github.com>
2024-02-28 20:30:53 -0500

    [refactor]isvalid

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
-    production: true,
-    serverUrl: 'http://ec2-18-191-191-125.us-east-2.compute.amazonaws.com:3000',
+    // production: true,
+    production: false,
+    // serverUrl: 'http://ec2-18-191-191-125.us-east-2.compute.amazonaws.com:3000',
+    serverUrl: 'http://localhost:3000',
 };
