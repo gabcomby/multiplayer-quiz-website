61fc0ba5df915147ce2c709b7c19c929969743b3
chapsag <goffi.pro@icloud.com>
2024-03-18 22:41:14 -0400

    Mise à jour des URL du serveur dans l'environnement de production

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://ec2-3-14-15-55.us-east-2.compute.amazonaws.com:3000/api',
-    socketUrl: 'http://ec2-3-14-15-55.us-east-2.compute.amazonaws.com:3000',
+    serverUrl: 'http://ec2-3-23-127-248.us-east-2.compute.amazonaws.com:3000/api',
+    socketUrl: 'http://ec2-3-23-127-248.us-east-2.compute.amazonaws.com:3000',
 };
