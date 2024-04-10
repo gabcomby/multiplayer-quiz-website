7ea77af63806741358df0ec7bb46b5ad0d094b2a
Gabriel Comby <combygabriel@gmail.com>
2024-04-10 16:05:12 -0400

    [BUGFIX|GC] Fixed server URL

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://ec2-3-23-127-248.us-east-2.compute.amazonaws.com:3000/api',
-    socketUrl: 'http://ec2-3-23-127-248.us-east-2.compute.amazonaws.com:3000',
+    serverUrl: 'http://ec2-18-220-228-190.us-east-2.compute.amazonaws.com:3000/api',
+    socketUrl: 'http://ec2-18-220-228-190.us-east-2.compute.amazonaws.com:3000',
 };
