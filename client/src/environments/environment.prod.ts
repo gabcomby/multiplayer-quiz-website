da413ec7687a709d9a983799db1a3d6f02537e84
Gabriel Comby <combygabriel@gmail.com>
2024-04-16 20:00:35 -0400

    [LINT|GC] Fixed lint issue

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -3,4 +3,3 @@ export const environment = {
     serverUrl: 'http://ec2-18-220-228-190.us-east-2.compute.amazonaws.com:3000/api',
     socketUrl: 'http://ec2-18-220-228-190.us-east-2.compute.amazonaws.com:3000',
 };
-
